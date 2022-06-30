f84ad47b175953806070e3aacb56558ba63c6884
Valentino <ghost085@mail.ru>
2022-06-30 19:31:46 +0300

    Text 1_1

--- 1_lesson/1_1/Program.cs
+++ 1_lesson/1_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, all people");
+﻿// string num_str = Console.ReadLine();
+// int num = int.Parse(num_str);
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine("да");
+
+}
+else
+{
+    Console.WriteLine("нет");
+
+
+}
+
+
